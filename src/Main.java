ca6ba8f622bf02b5a7917361d588926c26476d6a
Scott Pantall <aspantall@gmail.com>
2017-05-23 18:52:05 -0600

    Works on CodeEval

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,17 @@ public class Main {
     }
     
     public static void myOwnMod(String line) {
-        System.out.println(line);
+        String[] values = line.split(",");
+        int numerator = Integer.parseInt(values[0]);
+        int denominator = Integer.parseInt(values[1]);
+        int dividedAnswer = numerator / denominator;
+        
+        // Using modulus operator
+        //System.out.println("Using Mod Operator: " + numerator % denominator);
+        
+        System.out.println((numerator - (denominator * dividedAnswer)));
+        
+        
+        
     }
 }
